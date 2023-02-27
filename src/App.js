8702cf7fc9185d90e5f203192e5a444ce23c9335
Wojciech <wojciech.zarem@gmail.com>
2023-02-27 18:02:19 +0100

    start of generic room, before I destroy everything

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './navbar';
-import Bedroom from './bedroom';
+import Bedroom from './rooms/bedroom';
+import CourageView from './rooms/courageView';
+// import GenericRoom from './rooms/genericRoom';
+// import Hall from './rooms/hall';
+import { useState } from "react";
 
 function App() {
+  const [fadingOut, setFading] = useState(false);
+  function handleClick() {
+    setFading(!fadingOut)
+  }
   return (
-    <div className="App">
-      <header className="App-header">ererererrtrtrtrt
-      <Navbar />
-      </header>
-      <Bedroom />
-    </div>
+    <Router>
+      <div className="App" style={fadingOut ? {filter: 'blur(3px)'} : undefined }>
+        <header className="App-header" onClick={handleClick}>ererererrtrtrtrt
+        <Navbar />
+        </header>
+        <Routes>
+          <Route path="/bedroom" element={<Bedroom handleClick={handleClick} />} />
+          {/* <Route path="/hall" element={<Hall />} /> */}
+          <Route path="/courageView" element={<CourageView />} />
+          {/* <Route path="/genericRoom" element={<GenericRoom />} /> */}
+      </Routes>
+      </div>
+    </Router>
   );
 }
 
