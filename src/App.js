f4ef71092cf7748cbb7a670995b51342ca8d3c88
Wojciech <wojciech.zarem@gmail.com>
2023-03-14 23:42:29 +0100

    fade

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { createRef } from 'react'
 import { createRoot } from 'react-dom/client'
 import { 
-  BrowserRouter as Router,
    Route, 
    Routes, 
    Navigate, 
@@ -12,54 +11,45 @@ import {
 import GenericRoom from './rooms/genericRoom';
 import Navbar from './navbar';
 import Bedroom from './rooms/bedroom';
-import CourageView from './rooms/courageView';
 import Hall from './rooms/hall';
+import CourageView from './rooms/courageView';
 import { useState } from "react";
 import { CSSTransition, SwitchTransition } from 'react-transition-group'
 
 
-const routes = [
-  {path: '/bedroom', name: 'Bedroom', element: <Bedroom />, nodeRef: createRef()},
-  {path: '/hall', name: 'Hall', element: <Hall />, nodeRef: createRef()}
-]
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: routes.map((route) => ({
-      index: route.path === '/',
-      path: route.path === '/' ? undefined : route.path,
-      element: route.element
-    }))
-  }
-])
 
 function App() {
+  let location = useLocation();
+  const currentOutlet = useOutlet();
   const [fadingOut, setFading] = useState(false);
   function handleClick() {
     setFading(!fadingOut)
   }
-  const currentOutlet = useOutlet()
-  const { nodeRef } =
-    routes.find((route) => route.path === window.location) ?? {}
+  // const currentOutlet = useOutlet()
+  // const { nodeRef } =
+  //   routes.find((route) => route.path === window.location) ?? {}
   return (
-    <Router>
+    // <Router>
       <div className="App" style={fadingOut ? {filter: 'blur(3px)'} : undefined }>
         <header className="App-header" >ererererrtrtrtrt
         <Navbar />
         </header>
-        <Routes>
-          <Route exact path="/" element={<Navigate to="/bedroom" />}>
-            
-          </Route>
-          <Route path="/bedroom" element={<Bedroom handleClick={handleClick} />} />
-          <Route path="/hall" element={<Hall handleClick={handleClick}/>} />
-          <Route path="/courageView" element={<CourageView />} />
-          <Route path="/genericRoom" element={<GenericRoom />} />
-      </Routes>
+        <div className="container">
+          <SwitchTransition>
+            <CSSTransition key={location.pathname} timeout={800} classNames="fade" unmountOnExit>
+              <Routes location={location}>
+                <Route exact path="/" element={<Navigate to="/bedroom" />} />
+                <Route path="/bedroom" element={<Bedroom handleClick={handleClick} />} />
+                <Route path="/hall" element={<Hall handleClick={handleClick}/>} />
+                <Route path="/courageView" element={<CourageView />} />
+                <Route path="/genericRoom" element={<GenericRoom />} />
+              </Routes>
+            </CSSTransition>
+          </SwitchTransition>
+        </div>
       </div>
-    </Router>
+    // </Router>
   );
 }
 
