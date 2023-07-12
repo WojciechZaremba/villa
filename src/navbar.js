b0c57d2d13b492f4f128ef2a2e99849d76675296
Wojciech <wojciech.zarem@gmail.com>
2023-07-12 15:19:27 +0200

    navbar for backrooms

--- src/navbar.js
+++ src/navbar.js
@@ -22,15 +22,33 @@ const Navbar = ({backLinks}) => {
 
     return (
         <>
-        <nav className="navbar navContainer">
-            <Link className="navbar link" key={0} to="/" id="logo">HOMER</Link>
-            <Link className="navbar link" key={999} to="/bedroom" id="bedroomNav">bedroom</Link>
-            <Link className="navbar link" key={9999} to="/hall" id="hallNav">hall</Link>
-            <Link className="navbar link" key={998} to="/bathroom" id="bathroomNav">bathroom</Link>
-            <Link className="navbar link" key={9998} to="/storage" id="storageNav">storage</Link>
-        </nav>
-        <nav className="  backrooms">
-            {backLinksElem}
+        <nav className="navContainer">
+            <nav className="navbar navContainer">
+                <Link className="navbar link" key={0} to="/" id="logo">HOMER</Link>
+                <Link className="navbar link" key={999} to="/bedroom" id="bedroomNav">bedroom</Link>
+                <Link className="navbar link" key={9999} to="/hall" id="hallNav">hall</Link>
+                <Link className="navbar link" key={998} to="/bathroom" id="bathroomNav">bathroom</Link>
+                <Link className="navbar link" key={9998} to="/storage" id="storageNav">storage</Link>
+                <div className="dropdown">
+                    <a href="/gen0" onClick={"()=>window.location.pathname='/gen0'"} className="dropButton link" key="444">experimental</a>
+                        <div className="backrooms dropdownContent">
+                            {backLinksElem}
+                        </div>
+                </div>
+            </nav>
+            <nav className="navbar externalLinks navContainer">
+                <Link className="navbar link github" key={9996} to="https://github.com/WojciechZaremba/villa" target="_blank" id="storageNav">GitHub <svg x="0px" y="0px" viewBox="0 0 100 100" width="15" height="15">
+                    <path fill="currentColor" d="
+                    M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,
+                    0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z
+                    ">   
+                    </path>
+                    <polygon fill="currentColor" points="
+                    45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,
+                    14.9 62.8,22.9 71.5,22.9">
+                    </polygon>
+                </svg></Link>
+            </nav>
         </nav>
         </>
     );
